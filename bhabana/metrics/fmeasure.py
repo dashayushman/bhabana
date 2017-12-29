[bug fix] f measure averaging scheme

bhabana/metrics/fmeasure.py
@@ -10,4 +10,5 @@ class FMeasure(Metric):
         return self.calculate(pred, gt)
 
     def calculate(self, pred, gt):
-        return f1_score(np.argmax(gt, axis=1), np.argmax(pred, axis=1))
+        return f1_score(np.argmax(gt, axis=1), np.argmax(pred, axis=1),
+                        average="weighted")

bhabana/pipeline/embedding_ngram_cnn_rnn_classification.py
@@ -15,7 +15,7 @@ class EmbeddingNgramCNNRNNClassification(nn.Module):
                  cnn_dropout=0.5, rnn_dropout=0.5):
         super(EmbeddingNgramCNNRNNClassification, self).__init__()
         embedding_dims = embedding_dims if pretrained_word_vectors is None \
-            else pretrained_word_vectors.shape[-1]
+                                        else pretrained_word_vectors.shape[-1]
         self.rnn_hidden_size = rnn_hidden_size
         self.pipeline = nn.ModuleList([Embedding(vocab_size=vocab_size,
                                                  embedding_dims=embedding_dims,
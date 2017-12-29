b519d96df6ae0463432d5336834545b2546f2ec6
Ayushman Dash <dash.ayushman.99@gmail.com>
2017-12-29 15:31:31 +0100

    [bug fix] minor vocab initialization error

--- bhabana/trainer/master_trainer.py
+++ bhabana/trainer/master_trainer.py
@@ -41,9 +41,9 @@ def get_experiment_by_name(name):
 
 
 if args.name in THE_BOOK_OF_EXPERIMENTS:
+    end_at = args.end_at if args.end_at != 0 else \
+        len(THE_BOOK_OF_EXPERIMENTS[args.name])
     for i_c, config in enumerate(THE_BOOK_OF_EXPERIMENTS[args.name]):
-        end_at = args.end_at if args.end_at != 0 else \
-                                        len(THE_BOOK_OF_EXPERIMENTS[args.name])
         if i_c >= args.start_from-1 and i_c <= end_at:
             config["experiment_name"] = config["experiment_name"] + " _" + \
                                         args.name + "_" + str(i_c)
